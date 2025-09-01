7ea351a6303e2660a664adb490299b3255761d5f
Kamal Dolikay <kamaldolikay@gmail.com>
2025-08-31 20:38:21 -0700

    update(home): add desktop component in the homepage

--- src/features/home/components/home.view.tsx
+++ src/features/home/components/home.view.tsx
@@ -1,8 +1,12 @@
 import MenuBar from '@/ui/menu-bar/components/menu-bar.component';
+import Desktop from '@/ui/desktop/components/desktop.component';
 
 const HomeView: React.FC = () => {
   return (
-    <MenuBar />
+    <>
+      <MenuBar />
+      <Desktop />
+    </>
   );
 };
 
