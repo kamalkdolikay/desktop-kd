40d296d9cee122e5c6e53c28f4d421165ca0a8f4
Kamal Dolikay <kamaldolikay@gmail.com>
2025-08-31 20:30:42 -0700

    feat(menu-bar): add dispatch to set the height of menubar component

--- src/ui/menu-bar/components/menu-bar.component.tsx
+++ src/ui/menu-bar/components/menu-bar.component.tsx
@@ -1,31 +1,23 @@
-import { useCallback, useRef, useState, memo } from 'react';
+import { useCallback, useLayoutEffect, useRef, useState, memo } from 'react';
 import MenuBarView from './menu-bar.view';
-import useClock from '@/hooks/clock.hook';
-import useClickOutside from '@/hooks/click-outside.hook';
-import useBackgroundImagePreload from '@/hooks/bg-image-preload.hook';
+import useClock from '@/hooks/menu-bar/clock.hook';
+import useClickOutside from '@/hooks/menu-bar/click-outside.hook';
+import useBackgroundImagePreload from '@/hooks/menu-bar/bg-image-preload.hook';
 import { BACKGROUND_IMAGES } from '../constants/menu-bar.constants';
+import { setHeight } from '../state/menu-bar.slice';
+import { useAppDispatch } from '@/store/redux.store';
 
 const MenuBar: React.FC = () => {
   const [activeMenu, setActiveMenu] = useState<string | null>(null);
   const [currentBackgroundIndex, setCurrentBackgroundIndex] =
     useState<number>(0);
   const menuRefs = useRef<Record<string, HTMLDivElement | null>>({});
+  const menubarRef = useRef<HTMLDivElement>(null);
+  const dispatch = useAppDispatch();
   const currentTime = useClock();
 
   useClickOutside(menuRefs, () => setActiveMenu(null));
   useBackgroundImagePreload(BACKGROUND_IMAGES);
-
-  /*const handleClickOutside = useCallback((e: MouseEvent) => {
-    const target = e.target as Node;
-    if (!Object.values(menuRefs.current).some((ref) => ref?.contains(target))) {
-      setActiveMenu(null);
-    }
-  }, []);
-
-  useEffect(() => {
-    document.addEventListener('click', handleClickOutside);
-    return () => document.removeEventListener('click', handleClickOutside);
-  }, [handleClickOutside]);*/
 
   const toggleMenu = useCallback((menu: string) => {
     setActiveMenu((prev) => (prev === menu ? null : menu));
@@ -41,11 +33,18 @@ const MenuBar: React.FC = () => {
     document.body.style.background = `url(${BACKGROUND_IMAGES[newIndex]}) center / cover no-repeat fixed`;
   };
 
+  useLayoutEffect(() => {
+    if (menubarRef.current) {
+      dispatch(setHeight(menubarRef.current.offsetHeight));
+    }
+  }, [dispatch]);
+
   return (
     <MenuBarView
       activeMenu={activeMenu}
       toggleMenu={toggleMenu}
       menuRefs={menuRefs}
+      menubarRef={menubarRef}
       closeMenu={closeMenu}
       currentTime={currentTime}
       changeBackground={changeBackground}
