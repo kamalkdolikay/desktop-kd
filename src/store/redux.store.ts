7da8b9bfa9fba30ff4871c5e64c8e201a923d8e2
Kamal Dolikay <kamaldolikay@gmail.com>
2025-08-31 20:39:56 -0700

    feat(redux): add menubar state in redux store for global access

--- src/store/redux.store.ts
+++ src/store/redux.store.ts
@@ -3,12 +3,14 @@ import { useSelector, useDispatch } from 'react-redux';
 import type { TypedUseSelectorHook } from 'react-redux';
 import homeReducer from '@/features/home/state/home.slice';
 import portfolioReducer from '@/features/portfolio/state/portfolio.slice';
+import menuBarReducer from '@/ui/menu-bar/state/menu-bar.slice';
 
 // Configure the Redux store
 const store = configureStore({
   reducer: {
     home: homeReducer,
     portfolio: portfolioReducer,
+    menubar: menuBarReducer,
   },
 });
 
