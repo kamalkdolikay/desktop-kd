71f02996cfeb3dc880feed62cedbc83deebe133d
Kamal Dolikay <kamaldolikay@gmail.com>
2025-09-11 01:57:16 -0700

    update(home): add portfolio dialog component in homepage

--- src/features/home/components/home.view.tsx
+++ src/features/home/components/home.view.tsx
@@ -1,11 +1,13 @@
 import MenuBar from '@/ui/menu-bar/components/menu-bar.component';
 import Desktop from '@/ui/desktop/components/desktop.component';
+import PortfolioDialog from '@/ui/portfolio-dialog/component/portfolio-dialog.component';
 
 const HomeView: React.FC = () => {
   return (
     <>
       <MenuBar />
       <Desktop />
+      <PortfolioDialog />
     </>
   );
 };
